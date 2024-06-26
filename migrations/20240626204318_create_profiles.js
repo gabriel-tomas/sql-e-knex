4f576953bf5a96b85a50c70360d8e2024b4ea3b5
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-06-26 18:07:27 -0300

    profiles table with relation one-to-one with users and roles table

---
+++ migrations/20240626204318_create_profiles.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('profiles', (table) => {
+    table.increments('id').primary();
+    table.text('bio');
+    table.integer('user_id').unique().unsigned();
+    table.foreign('user_id')
+      .references('users.id')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('profiles');
+};
